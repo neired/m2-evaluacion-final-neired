8953a2e76e96e2c8400a3ddf1ab4608807ac44d3
neired <nmillans91@gmail.com>
2019-08-08 20:17:17 +0200

    Create array for favorite shows

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,49 +3,17 @@
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const series = document.querySelector('.series__results');
-let favorites = document.querySelector('.favs__results');
+let favoritesList = document.querySelector('.favs__results');
 const favsContainer = document.querySelector('.favs');
 const endpoint = 'http://api.tvmaze.com/search/shows?q=';
 let newLi = '';
 let newH3 = '';
 let newImage = '';
-
-function getSeries () {
-// 1- se conecte con la API y me devuelva series
-// 2- coja el texto del input y lo añada al final de la URL
-  resetSearch();
-  fetch(endpoint + input.value)
-    .then(response => response.json())
-    .then(data => {
-      // 3- me devuelva esas series y las pinte en un <li> para cada serie, con una <img> y un <h3>
-      for (let item of data) {
-        newLi = document.createElement('li');
-        newLi.classList.add('show__item');
-        newH3 = document.createElement('h3');
-        newImage = document.createElement('img');
-        series.appendChild(newLi);
-        newLi.appendChild(newImage);
-        newLi.appendChild(newH3);
-        newH3.innerHTML = item.show.name;
-        // 4- si alguna serie no tiene <img>, que le añada una por defecto
-        if (item.show.image !== null) {
-          newImage.src = item.show.image.medium;
-          newImage.alt = item.show.name;
-        } else {
-          newImage.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-          newImage.alt = 'Default empty image';
-        }
-        newLi.addEventListener('click', getFavs);
-      }
-    });
-}
-btn.addEventListener('click', getSeries);
+const favorites = [];
 
 function resetSearch () {
-  // 5- cuando haga una nueva búsqueda no quiero tener las anteriores en pantalla
   series.innerHTML = '';
 }
-
 function searchWithEnter() {
   if(event.keyCode===13) {
     getSeries();
@@ -53,25 +21,86 @@ function searchWithEnter() {
 }
 input.addEventListener('keypress', searchWithEnter);
 
-//crear un array de pelis favoritas
-// 1- cambiar los estilos de fondo y color de fuente a la que seleccione
-// 2- moverlos a otra lista
-function getFavs (){
-  event.currentTarget.classList.toggle('show__fav');
+function getSeries () {
+  resetSearch();
+  fetch(endpoint + input.value)
+    .then(response => response.json())
+    .then(data => {
+      // 3- me devuelva esas series y las pinte en un <li> para cada serie, con una <img> y un <h3>
+      for (let item of data) {
+        let showName = item.show.name;
+        let showImage = item.show.image;
+        newLi = document.createElement('li');
+        newLi.classList.add('show__item');
+        newLi.setAttribute('data-name', showName);
+        newH3 = document.createElement('h3');
+        newH3.classList.add('show__item-name');
+        newImage = document.createElement('img');
+        newImage.classList.add('show__item-img');
+        series.appendChild(newLi);
+        newLi.appendChild(newImage);
+        newLi.appendChild(newH3);
+        newH3.innerHTML = showName;
+        // 4- si alguna serie no tiene <img>, que le añada una por defecto
+        if (showImage !== null) {
+          newImage.src = showImage.medium;
+          newImage.alt = showName;
+        } else {
+          newImage.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+          newImage.alt = `${showName} image`;
+        }
+        selectFav ();
+        // newLi.addEventListener('click', getFavs);
+      }
+    });
+}
+btn.addEventListener('click', getSeries);
+
+// 3- moverlos a otra lista
+
+// function getFavs (){
+//   console.log(event.currentTarget);
+//   event.currentTarget.classList.toggle('show__fav');
+//   favsContainer.classList.remove('hidden');
+//   addFav();
+// }
+function addFav () {
+  // 1- cambiar los estilos de fondo y color de fuente a la que seleccione como favorita
+  const currentShow = event.currentTarget;
+  currentShow.classList.toggle('show__fav');
   favsContainer.classList.remove('hidden');
-  addFav();
+  const favShowName = currentShow.getAttribute('data-name');
+  // 2- hacer un array con las pelis favoritas
+  if (currentShow.classList.contains('show__fav') === true) {
+    //si lo he marcado como fav, al array (si no está ya de antes, claro)
+    if (favorites.includes(favShowName) !== true){
+      favorites.push(favShowName);
+      let newFavLi = document.createElement('li');
+      // newFavLi.classList.add('show__item_fav');
+      favoritesList.appendChild(newFavLi);
+      newFavLi.innerHTML = event.currentTarget.innerHTML;
+// }
+    }
+  } else {
+    //si ya está de antes y lo estoy re-marcando, me lo borras
+    const i = favorites.indexOf(favShowName);
+    if (i > -1) {
+      favorites.splice(i, 1);
+    }
+  }
+  console.log(favorites);
+}
+//hace un array con las pelis que salen y les pone un listener a todas para marcarlas como favs
+function selectFav () {
+  const myShows = document.querySelectorAll('.show__item');
+  for (const show of myShows) {
+    show.addEventListener('click', addFav);
+  }
 }
 
-function addFav () {
-  let newFavLi = document.createElement('li');
-  let newFavImage = document.createElement('image');
-  let newFavH3 = document.createElement('h3');
-  newFavLi.classList.add('show__item_fav');
-  favorites.appendChild(newFavLi);
-  newFavLi.appendChild(newFavImage);
-  newFavLi.appendChild(newFavH3);
-  newFavH3.innerHTML = event.currentTarget.innerHTML;
-}
-function removeFav () {
-  favorites.remove();
-}
+//   {
+//   let newFavLi = document.createElement('li');
+//   newFavLi.classList.add('show__item_fav');
+//   favorites.appendChild(newFavLi);
+//   newFavLi.innerHTML = event.currentTarget.innerHTML;
+// }
