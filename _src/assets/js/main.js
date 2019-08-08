1a2a3077d541008048fbafcc8b4708209ce5a229
neired <nmillans91@gmail.com>
2019-08-08 12:28:40 +0200

    Show all series on page

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,24 +6,30 @@ const series = document.querySelector('.series__results');
 const container = document.querySelector('.series__container');
 const endpoint = 'http://api.tvmaze.com/search/shows?q=';
 
-function getSeries (event) {
+function getSeries () {
 // 1- se conecte con la API y me devuelva series
 // 2- coja el texto del input y lo añada al final de la URL
   fetch(endpoint + input.value)
     .then(response => response.json())
     .then(data => {
-      console.log(data['']);
-      // 3- me devuelva esas series y las pinte en la página
-      // 4- cree un <li> para cada serie, con una <img> y un <h3>
-      // 5- si alguna serie no tiene <img>, que le añada una por defecto
-      for (item of data) {
+      // 3- me devuelva esas series y las pinte en un <li> para cada serie, con una <img> y un <h3>
+      for (let item of data) {
         const newLi = document.createElement('li');
+        newLi.classList.add('show__style');
         const newH3 = document.createElement('h3');
         const newImage = document.createElement('img');
         series.appendChild(newLi);
-        newLi.appendChild(newH3);
         newLi.appendChild(newImage);
-        newLi.appendChild(item);
+        newLi.appendChild(newH3);
+        newH3.innerHTML = item.show.name;
+        // 4- si alguna serie no tiene <img>, que le añada una por defecto
+        if (item.show.image !== null) {
+          newImage.src = item.show.image.medium;
+          newImage.alt = item.show.name;
+        } else {
+          newImage.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+          newImage.alt = 'Default empty image';
+        }
       }
     });
 }
