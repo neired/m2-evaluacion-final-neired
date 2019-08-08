7c59cdb978c8d589d273bba71d4659791cae02be
neired <nmillans91@gmail.com>
2019-08-08 13:45:14 +0200

    Change color of selected shows

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,21 +3,26 @@
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const series = document.querySelector('.series__results');
+const favoritas = document.querySelector('.favs__results');
 // const container = document.querySelector('.series__container');
 const endpoint = 'http://api.tvmaze.com/search/shows?q=';
+let newLi = '';
+let newH3 = '';
+let newImage = '';
 
 function getSeries () {
 // 1- se conecte con la API y me devuelva series
 // 2- coja el texto del input y lo añada al final de la URL
+  resetSearch();
   fetch(endpoint + input.value)
     .then(response => response.json())
     .then(data => {
       // 3- me devuelva esas series y las pinte en un <li> para cada serie, con una <img> y un <h3>
       for (let item of data) {
-        const newLi = document.createElement('li');
-        newLi.classList.add('show__style');
-        const newH3 = document.createElement('h3');
-        const newImage = document.createElement('img');
+        newLi = document.createElement('li');
+        newLi.classList.add('show__item');
+        newH3 = document.createElement('h3');
+        newImage = document.createElement('img');
         series.appendChild(newLi);
         newLi.appendChild(newImage);
         newLi.appendChild(newH3);
@@ -30,27 +35,27 @@ function getSeries () {
           newImage.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
           newImage.alt = 'Default empty image';
         }
+        newLi.addEventListener('click', getFavs);
       }
     });
 }
-// 5- cuando haga una nueva búsqueda no quiero tener las anteriores en pantalla
+btn.addEventListener('click', getSeries);
+
 function resetSearch () {
+  // 5- cuando haga una nueva búsqueda no quiero tener las anteriores en pantalla
   series.innerHTML = '';
 }
-function getNewSearch () {
-  if (series.innerHTML === '') {
-    getSeries();
-  } else {
-    resetSearch();
-    getSeries();
-  }
-}
-btn.addEventListener('click', getNewSearch);
 
-function compareWithEnter() {
+function searchWithEnter() {
   if(event.keyCode===13) {
-    getNewSearch();
+    getSeries();
   }
 }
-input.addEventListener('keypress', compareWithEnter);
+input.addEventListener('keypress', searchWithEnter);
 
+//crear un array de pelis favoritas
+// 1- cambiar los estilos de fondo y color de fuente a la que seleccione
+// 2- moverlos a otra lista
+function getFavs (){
+  event.currentTarget.classList.toggle('show__fav');
+}
